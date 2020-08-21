8f44cbce9c83701b3bb41eacec99cd6d6d2102e9
qiujin123 <qiujinzou123@gmail.com>
2020-08-21 17:18:16 -0400

    created flipping cards

---
+++ dev/js/cards-flip.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll(".card");
+for ( var i = 0; i < cards.length; i++ ) {
+  const selectedCard = cards[i];
+  cardFlip(selectedCard);
+}
+
+function cardFlip(selectedCard) {
+  selectedCard.addEventListener("click", function() {
+    const cardState = selectedCard.classList;
+    cardState.toggle("is-flipped");
+  });
+}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,6 @@
 
 // @codekit-prepend "scroll-trigger.js";
+// @codekit-prepend "cards-flip.js";
 
 
 //console.log("does this work")
