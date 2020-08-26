b12e85b4dc5cdb8b869a76ec362d8886c7919786
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-08-26 15:28:37 -0400

    line progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,5 +2,6 @@
 // @codekit-prepend "scroll-trigger.js";
 // @codekit-prepend "cards-flip.js";
 // @codekit-prepend "accordion.js";
+// @codekit-prepend "drawsvg-sec2.js";
 
 //console.log("does this work")
