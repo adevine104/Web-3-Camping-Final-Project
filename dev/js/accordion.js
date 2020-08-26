3112e27985f67ab28eeadf7cc4b5b97da3c9d9ee
ninapagano <63386466+ninapagano@users.noreply.github.com>
2020-08-26 07:37:48 -0700

    fixed accordion

--- dev/js/accordion.js
+++ dev/js/accordion.js
@@ -1,7 +1,6 @@
-import { gsap } from 'gsap'
 
 gsap.set(".bottom",{height:0});
-gsap.set(".fas",{transformOrgin:"center"});
+gsap.set(".fas",{transformOrigin:"center"});
 
 var contentHeight;
 
@@ -9,15 +8,15 @@ $(".option") .on("click", function(){
 
     contentHeight = $(this).children(".bottom").find('.content').outerHeight();
 
-    const option = document.querySelector('option')
+    const option = document.querySelector('.option');
 
     if(option.dataset.accordionToggle === "true"){
 
         gsap.to(".bottom", {duration:0.25,height: 0});
-        gsap.to(".bott", {duration:0.1,rotation: 0});
+        gsap.to(".fas", {duration:0.1,rotation: 0});
     }
 
-    if($(this).children(".bottom").height()>0){
+    if($(this).children(".bottom").height() > 0 ){
 
         gsap.to($(this).children(".bottom"), {duration:0.25, height: 0});
         gsap.to($(this).children(".top").children(".fas"), {duration:0.25, rotation: 0});
