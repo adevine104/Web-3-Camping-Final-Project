10ff24f112677e7afb341ae36ad5dfd777b45ee7
qiujin123 <qiujinzou123@gmail.com>
2020-08-26 00:38:10 -0400

    added bear animation card 3

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,8 @@
 
 // @codekit-prepend "scroll-trigger.js";
 // @codekit-prepend "cards-flip.js";
+// @codekit-prepend "cards-animation.js";
+// @codekit-prepend "cards-events.js";
 
 
 //console.log("does this work")
