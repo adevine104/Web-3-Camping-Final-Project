0b2e38f826480e7a95b1a80f8dcce06a30a78672
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-08-28 15:39:41 -0400

    fixed??

---
+++ dev/js/drawsvg-sec2.js
@@ -0,0 +1,36 @@
+
+// console.log("drawsvg-working");
+
+gsap.registerPlugin(DrawSVGPlugin);
+
+var svgLineTimeLine = gsap.timeline({
+});
+
+svgLineTimeLine.from("#Path",{duration:2, drawSVG:"0%"}, "playsvg");
+
+
+ScrollTrigger.create({
+    animation: svgLineTimeLine,
+    toggleActions: "play none complete none",
+    trigger: "#section-2",
+    start: "top 20%",
+    end: "bottom 65%",
+    //end: "bottom 40%",
+    // markers: true,
+    // id: "card",
+    scrub: 8
+});
+
+// svgLineTimeLine.from("#Path-2",{duration:1, drawSVG:"0%", opacity: 0}, "playsvg2")
+
+
+// ScrollTrigger.create({
+//     animation: svgLineTimeLine,
+//     toggleActions: "play none none none",
+//     trigger: "#Path-2",
+//     start: "top 50%",
+//     //end: "bottom 40%",
+//     markers: true, 
+//     // id: "card",
+//     scrub: 1
+// });
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,17 @@
 
+// @codekit-prepend "tinySlider.js";
 // @codekit-prepend "scroll-trigger.js";
 // @codekit-prepend "cards-flip.js";
+// @codekit-prepend "cards-events.js";
+// @codekit-prepend "cards-animation.js";
 // @codekit-prepend "accordion.js";
+// @codekit-prepend "drawsvg-sec2.js";
+
+// @codekit-prepend "main-nav/burger-animation.js";
+// @codekit-prepend "main-nav/burger-mouse-events.js";
+// @codekit-prepend "main-nav/nav-global.js";
+// @codekit-prepend "main-nav/burger-click.js";
+// @codekit-prepend "main-nav/nav-scroll.js";
+// @codekit-prepend "main-nav/main-nav-click.js";
 
 //console.log("does this work")
