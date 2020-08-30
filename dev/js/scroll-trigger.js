d4619f26445376214b80e659cc0eb79e183abc1b
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-08-30 15:06:20 -0400

    content-spacing and arrows

--- dev/js/scroll-trigger.js
+++ dev/js/scroll-trigger.js
@@ -1,9 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#arrow", {duration:2, alpha:0, yPercent:-100,
+gsap.from("#arrow-1", {duration:2, alpha:0, yPercent:-100,
 
     scrollTrigger:{
-        trigger:"#arrow",
+        trigger:"#arrow-1",
+        start: "top 80%",
+        end: "bottom 80%",
+        toggleActions: "restart none none none"
+        // markers: true
+        //id:"portfolio"
+    }})
+
+    gsap.from("#arrow", {duration:2, alpha:0, yPercent:-100,
+
+    scrollTrigger:{
+        trigger: "#arrow",
         start: "top 80%",
         end: "bottom 80%",
         toggleActions: "restart none none none"
