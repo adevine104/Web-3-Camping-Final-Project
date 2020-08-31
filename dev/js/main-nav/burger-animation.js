ae3b7518e1730e4fcb2ffe597e8c4b50a2c27688
blairda <bda34@drexel.edu>
2020-08-31 14:30:53 -0400

    Change nav colors, fix html errors

--- dev/js/main-nav/burger-animation.js
+++ dev/js/main-nav/burger-animation.js
@@ -1,48 +1,6 @@
 // var burgerAnimationTimeLine = gsap.timeline({paused:true});
 var burgerAnimateSpeed = 0.25;
 
-// //reset the transformOrigin point for each line
-// gsap.set(".lines",{transformOrigin:"center"});
-
-// //1: first is the reference to the timeline (burgerAnimationTimeLine ), 2: what do you want to animate? 3{ what properties to do you want animate?}
-// burgerAnimationTimeLine.to("#middle-line",{duration:burgerAnimateSpeed,alpha:0},"burgerStart")
-//                         .to("#top-line",{duration:burgerAnimateSpeed,rotation:45, y:13, stroke: "#000"},"burgerStart")
-//                         .to("#bottom-line",{duration:burgerAnimateSpeed,rotation:-45, y:-13, stroke: "#000", onReverseComplete:resetBurgerLinecsolor},"burgerStart")
-//                         .to("#right-arrow",{duration:burgerAnimateSpeed, alpha:0, rotation:0, stroke: "#000"}, "burgerToX")
-//                         .to("#left-arrow",{duration:burgerAnimateSpeed, alpha:0, rotation:0, stroke: "#000"}, "burgerToX")
-//                         .to("#circle",{duration:burgerAnimateSpeed, alpha:1, rotation:0, y:-6.5, x:-5, stroke: "#000"}, "burgerToX");
-
-
-
-// function animateBurger(){
-//     // check the  canYouSeeTheMenu bool valus
-//     if(canYouSeeTheMenu === true){
-//         // turn the burger into an X
-//         burgerAnimationTimeLine.play();
-//     }else{
-//         // turn the X into a burger
-//         burgerAnimationTimeLine.reverse();
-//     }
-// }
-
-// // function to change the color of hte burger lines back to red, only called on the compete timeline reverse of burgerAnimationTimeLine
-// function resetBurgerLinecsolor(){
-//     //change the burger lines back to red over 0.25 seconds
-//     gsap.to(".lines",{duration:0.25, stroke:"#000"});
-// }
-
-
-// // function animateX(){
-// //     // check the  canYouSeeTheMenu bool valus
-// //     if(burgerAnimationTimeLine === true){
-// //         // turn the burger into an X
-// //         XAnimationTimeLine.play();
-// //     }else{
-// //         // turn the X into a burger
-// //         XAnimationTimeLine.reverse();
-// //     }
-// // }
-
 
 var burgerAnimationTimeLine = gsap.timeline({
     paused: true
--- dev/js/main-nav/nav-scroll.js
+++ dev/js/main-nav/nav-scroll.js
@@ -11,19 +11,3 @@ function scrollIndex(){
  // gsap.to(window, {duration: 2, scrollTo:"#index-card1"});
  gsap.to(window, {duration: 2, scrollTo: {y: scrollItems[$('#main-nav ul li button').index(this)], offsetY: 100}});
 }
-
-// console.log(scrollItems[5]);
-
-// function scrollIndex(){
-// //     gsap.to(window, {duration: 2, scrollTo:"#sec-2-btn"});
-//     gsap.to(window, {duration: 2, scrollTo:{ y: scrollItems[$('#main-nav ul li button').index(this)], offsetY: 60}});
-// }
-
-// // function scrollSection2(){
-// //     // gsap.to(window, {duration: 2, scrollTo:"#sec-2-btn"});
-// //     gsap.to(window, {duration: 2, scrollTo:{y:"#sec-2-btn", offsetY:60}});
-// // }
-
-// // function scrollSection2(){
-// //     gsap.to(window, {duration: 2, scrollTo:"#sec-3-btn"});
-// // }
