068608c870697cf05d8299ce81a858c3b9cdff1a
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-08-31 21:42:03 -0400

    line media queries

--- dev/js/drawsvg-sec2.js
+++ dev/js/drawsvg-sec2.js
@@ -6,19 +6,19 @@ gsap.registerPlugin(DrawSVGPlugin);
 var svgLineTimeLine = gsap.timeline({
 });
 
-svgLineTimeLine.from("#Path",{duration:2, drawSVG:"0%"}, "playsvg");
+svgLineTimeLine.from("#Path",{duration:3, drawSVG:"0%"}, "playsvg");
 
 
 ScrollTrigger.create({
     animation: svgLineTimeLine,
     toggleActions: "play none complete none",
     trigger: "#section-2",
-    start: "top 20%",
-    end: "bottom 65%",
+    start: "top 35%",
+    end: "bottom 60%",
     //end: "bottom 40%",
     // markers: true,
     // id: "card",
-    scrub: 8
+    scrub: 15
 });
 
 // svgLineTimeLine.from("#Path-2",{duration:1, drawSVG:"0%", opacity: 0}, "playsvg2")
