9ff5fc329af6065cee1b08a42c94677a5d2cd856
blairda <bda34@drexel.edu>
2020-09-03 17:53:13 -0400

    Correct nav scroll destinations

--- dev/js/main-nav/nav-scroll.js
+++ dev/js/main-nav/nav-scroll.js
@@ -1,11 +1,8 @@
 
 gsap.registerPlugin(ScrollToPlugin);
 
-var scrollItems =["#section-2", "#section-3", "#section-4", "#section-5", "#section-6"];
+var scrollItems =["#section-2", "#section-4", "#section-3", "#section-5", "#section-6"];
 
-
-// animating three buttons to scrollto respective index sections
-// var scrollItems =["#index-card1", "#index-card2", "#index-card3"];
  
 function scrollIndex(){
  // gsap.to(window, {duration: 2, scrollTo:"#index-card1"});
